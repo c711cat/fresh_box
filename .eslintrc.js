da84fc98c862ae637c4a5e9688f47a44b42d1d33
c711cat <c711cat@gmail.com>
2024-01-30 14:17:25 +0800

    針對vs code報錯處裡<!-- eslint-disable vue/multi-word-component-names -->

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,8 @@
 module.exports = {
   root: true,
   env: {
-    browser: true,
     node: true,
+    es6: true,
   },
   extends: ["plugin:vue/essential", "eslint:recommended"],
   parserOptions: {
@@ -10,6 +10,16 @@ module.exports = {
   },
   plugins: [],
   rules: {
-    "vue/multi-word-component-names": "off",
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "vue/no-multiple-template-root": 0,
   },
+  overrides: [
+    {
+      files: ["**/__test__/*.{j,t}?(x)}", "**/tests/unit/**/*.spec.{j,t}s?(x)"],
+      env: {
+        jest: true,
+      },
+    },
+  ],
 };
