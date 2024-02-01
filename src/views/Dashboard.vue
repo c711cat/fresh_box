72ff597eecd3ed2e0c08a3c643cedd1a9c8b25e0
c711cat <c711cat@gmail.com>
2024-02-01 16:52:20 +0800

    加入驗證token的api

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,19 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template><div>dashboard</div></template>
+<script>
+import Cookie from "js-cookie";
+
+export default {
+  data() {
+    return {};
+  },
+  created() {
+    const token = Cookie.get("freshBoxToken");
+    this.$http.defaults.headers.common["Authorization"] = token;
+    const api = `${process.env.VUE_APP_API}api/user/check`;
+    this.$http.post(api).then((res) => {
+      console.log(res);
+    });
+  },
+};
+</script>
