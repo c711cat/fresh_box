6e04f4ab3dce46ce49943ca7d03cbbbb224b0fbd
c711cat <c711cat@gmail.com>
2024-02-29 23:24:10 +0800

    update navbar title

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,7 +2,7 @@
 <template>
   <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Navbar</a>
+      <a class="navbar-brand" href="#">Flash Box</a>
       <button
         class="navbar-toggler"
         type="button"
