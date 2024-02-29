baafd8e35ffced0b25010711c89824b7ed2dae5d
c711cat <c711cat@gmail.com>
2024-02-29 23:22:12 +0800

    先貼上範例UI測試是否成功開啟modal

---
+++ src/components/DelModal.vue
@@ -0,0 +1,67 @@
+<template>
+  <div ref="delModal" class="modal" tabindex="-1">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Modal title</h5>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="modal-body">
+          <p>Modal body text goes here.</p>
+        </div>
+        <div class="modal-footer">
+          <button
+            type="button"
+            class="btn btn-secondary"
+            data-bs-dismiss="modal"
+          >
+            Close
+          </button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import Modal from "bootstrap/js/dist/modal";
+
+export default {
+  data() {
+    return {
+      modal: {},
+      tempProduct: {},
+    };
+  },
+  props: {
+    product: {
+      type: Object,
+      default() {
+        return {};
+      },
+    },
+  },
+  watch: {
+    product() {
+      this.tempProduct = this.product;
+    },
+  },
+  methods: {
+    showModal() {
+      this.modal.show();
+    },
+    hideModal() {
+      this.modal.hide();
+    },
+  },
+  mounted() {
+    this.modal = new Modal(this.$refs.delModal);
+    this.showModal();
+  },
+};
+</script>
