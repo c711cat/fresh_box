8f4a049b0a387579c1c1385e3e3094d8993ab06e
c711cat <c711cat@gmail.com>
2024-03-04 14:05:12 +0800

    將產品列表整個包進一個div裡，與Loading元件區別開來，再使用if/else判斷顯示

--- src/views/Admin'sProducts.vue
+++ src/views/Admin'sProducts.vue
@@ -1,83 +1,86 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
   <Loading v-if="isLoading" />
-  <div class="d-flex justify-content-between align-items-center">
-    <h3 class="titleStyle py-4 mb-0">後台產品列表</h3>
-    <div>
-      <button
-        @click="openModal(true)"
-        type="button"
-        class="btn btn-outline-primary me-5"
-      >
-        新增產品
-      </button>
-    </div>
-  </div>
-  <div class="container">
-    <div
-      v-for="item in products"
-      :key="item.id"
-      class="row align-items-center border-top py-4"
-    >
-      <div class="text-center col-12 col-sm-2">
-        <img :src="item.imageUrl" class="img-fluid" />
+  <div v-else>
+    <div class="d-flex justify-content-between align-items-center">
+      <h3 class="titleStyle py-4 mb-0">後台產品列表</h3>
+      <div>
+        <button
+          @click="openModal(true)"
+          type="button"
+          class="btn btn-outline-primary me-5"
+        >
+          新增產品
+        </button>
       </div>
-      <div class="col-12 col-sm-10">
-        <div class="row align-items-center">
-          <div class="col-12 col-sm-6 col-lg-3">
-            <div>{{ item.title }}</div>
-            <div>{{ item.content }}</div>
-          </div>
-          <div class="col-4 col-sm-3 col-lg">
-            <div>原價</div>
-            <div>NT$ {{ item.origin_price }}</div>
-          </div>
-          <div class="col-4 col-sm-3 col-lg">
-            <div>售價</div>
-            <div>NT$ {{ item.price }}</div>
-          </div>
-          <div class="col-4 col-sm-6 col-lg">
-            數量：{{ item.num }} {{ item.unit }}
-          </div>
-          <div
-            v-if="item.is_enabled"
-            class="col-8 col-sm-3 col-lg text-success"
-          >
-            販售中
-          </div>
-          <div v-else class="col-8 col-sm-3 col-lg text-secondary">未販售</div>
-          <div
-            class="col-auto col-sm-3 col-lg-auto d-flex justify-content-start flex-wrap"
-          >
-            <button
-              @click="openModal(false, item)"
-              type="button"
-              class="btn btn-outline-primary rounded-1 me-1 my-1"
+    </div>
+    <div class="container">
+      <div
+        v-for="item in products"
+        :key="item.id"
+        class="row align-items-center border-top py-4"
+      >
+        <div class="text-center col-12 col-sm-2">
+          <img :src="item.imageUrl" class="img-fluid" />
+        </div>
+        <div class="col-12 col-sm-10">
+          <div class="row align-items-center">
+            <div class="col-12 col-sm-6 col-lg-3">
+              <div>{{ item.title }}</div>
+              <div>{{ item.content }}</div>
+            </div>
+            <div class="col-4 col-sm-3 col-lg">
+              <div>原價</div>
+              <div>NT$ {{ item.origin_price }}</div>
+            </div>
+            <div class="col-4 col-sm-3 col-lg">
+              <div>售價</div>
+              <div>NT$ {{ item.price }}</div>
+            </div>
+            <div class="col-4 col-sm-6 col-lg">
+              數量：{{ item.num }} {{ item.unit }}
+            </div>
+            <div
+              v-if="item.is_enabled"
+              class="col-8 col-sm-3 col-lg text-success"
             >
-              編輯
-            </button>
-            <button
-              @click="openDelModal(item)"
-              type="button"
-              class="btn btn-outline-danger rounded-1 me-1 my-1"
+              販售中
+            </div>
+            <div v-else class="col-8 col-sm-3 col-lg text-secondary">
+              未販售
+            </div>
+            <div
+              class="col-auto col-sm-3 col-lg-auto d-flex justify-content-start flex-wrap"
             >
-              刪除
-            </button>
+              <button
+                @click="openModal(false, item)"
+                type="button"
+                class="btn btn-outline-primary rounded-1 me-1 my-1"
+              >
+                編輯
+              </button>
+              <button
+                @click="openDelModal(item)"
+                type="button"
+                class="btn btn-outline-danger rounded-1 me-1 my-1"
+              >
+                刪除
+              </button>
+            </div>
           </div>
         </div>
       </div>
     </div>
+    <ProductModal
+      ref="productModal"
+      :product="tempProduct"
+      @edit-product="editProduct"
+      @add-product="addProduct"
+    >
+    </ProductModal>
+    <DelModal ref="delModal" :product="tempProduct" @del-product="delProduct">
+    </DelModal>
   </div>
-
-  <ProductModal
-    ref="productModal"
-    :product="tempProduct"
-    @edit-product="editProduct"
-    @add-product="addProduct"
-  >
-  </ProductModal>
-  <DelModal ref="delModal" :product="tempProduct" @del-product="delProduct">
-  </DelModal>
 </template>
 
 <script>
