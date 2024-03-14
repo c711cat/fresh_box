4d6ace80847e863b2f41eb98f82463c149a48b8f
c711cat <c711cat@gmail.com>
2024-03-14 14:36:44 +0800

    補上編輯的訊息回饋轉換成中文

--- src/methods/PushMsg.js
+++ src/methods/PushMsg.js
@@ -45,6 +45,42 @@ export default function (res, title) {
         item = "啟用";
         msg.push(item);
       }
+      if (item === "code 屬性不得為空") {
+        item = "折扣碼";
+        msg.push(item);
+      }
+      if (item === "percent 屬性不得為空") {
+        item = "總金額折扣％";
+        msg.push(item);
+      }
+      if (item === "due_date 屬性不得為空") {
+        item = "使用期限";
+        msg.push(item);
+      }
+      if (item === "due_date 型別錯誤") {
+        item = "使用期限";
+        msg.push(item);
+      }
+      if (item === "title 屬性不得為空") {
+        item = "名稱";
+        msg.push(item);
+      }
+      if (item === "unit 屬性不得為空") {
+        item = "單位";
+        msg.push(item);
+      }
+      if (item === "origin_price 屬性不得為空") {
+        item = "原價";
+        msg.push(item);
+      }
+      if (item === "price 屬性不得為空") {
+        item = "售價";
+        msg.push(item);
+      }
+      if (item === "category 型別錯誤") {
+        item = "類別";
+        msg.push(item);
+      }
     });
     emitter.emit("push-message", {
       style: "failure",
