b110424806c5721fae603ae3a5630f28405b483a
c711cat <c711cat@gmail.com>
2024-03-19 09:35:48 +0800

    建立user的dashboard

---
+++ src/views/UserDashboard.vue
@@ -0,0 +1,26 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <Navbar></Navbar>
+  <div>
+    <Messages></Messages>
+    <router-view />
+  </div>
+</template>
+
+<script>
+import Navbar from "../components/Navbar.vue";
+import emitter from "@/methods/emitter";
+import Messages from "@/components/Messages.vue";
+
+export default {
+  data() {
+    return {};
+  },
+  provide() {
+    return {
+      emitter,
+    };
+  },
+  components: { Navbar, Messages },
+};
+</script>
