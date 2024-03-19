39cd36227f203eb6bbc8914216f8b1023c88305c
c711cat <c711cat@gmail.com>
2024-03-19 09:37:27 +0800

    建立消費者觀看商品頁面的UI

---
+++ src/views/UserProducts.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="card" style="width: 18rem">
+    <img
+      src="https://images.unsplash.com/photo-1629367308496-a2496ba22f88?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      class="card-img-top"
+    />
+    <div class="card-body">
+      <h5 class="card-title">燕巢珍珠芭樂</h5>
+      <p class="card-text">600g±10% / 袋</p>
+      <div class="d-flex justify-content-between">
+        <p class="card-text text-secondary">原價 89元</p>
+        <strong class="card-text text-danger fs-5">特價 79元</strong>
+      </div>
+
+      <div
+        class="btn-group d-flex"
+        role="group"
+        aria-label="Basic outlined example"
+      >
+        <button type="button" class="btn btn-outline-primary disabled">
+          移除
+        </button>
+        <button type="button" class="btn btn-outline-primary">
+          加入購物車
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
