dda26c26c5ca7520d0448da87818aed0016eb38b
c711cat <c711cat@gmail.com>
2024-03-28 00:04:25 +0800

    調整RWD排版

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -1,13 +1,13 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <div class="m-3">
+  <div class="m-5">
     <h3 class="ps-2">購物車清單</h3>
     <div
       v-for="(item, index) in carts"
       :key="index"
       class="row m-0 justify-content-center align-items-center border-top pt-3 pb-3"
     >
-      <div class="col-1 col-md-1 text-center">
+      <div class="col-12 col-sm-1 col-md-1 text-center p-0">
         <button
           @click="delItem(item)"
           type="button"
@@ -18,34 +18,38 @@
       </div>
 
       <img
-        class="imgBody col-2 col-md-2 col-lg-1 col-xl-1"
+        class="imgBody col-6 col-sm-2 col-md-2 col-lg-2 col-xl-1 my-1"
         :src="item.product.imageUrl"
         alt=""
       />
 
-      <div class="col-4 col-md-3 col-lg-6 col-xl-5 row m-0 align-items-center">
-        <div class="col-md-12 col-lg-4">{{ item.product.title }}</div>
-        <div class="col-md-12 col-lg-4">
+      <div
+        class="col-12 col-sm-5 col-md-4 col-lg-3 col-xl-5 row mx-0 my-1 align-items-center text-sm-start text-center"
+      >
+        <div class="col-md-12 col-lg-12 col-xl-4">{{ item.product.title }}</div>
+        <div class="col-md-12 col-lg-12 col-xl-4">
           {{ item.product.content }}／{{ item.product.unit }}
         </div>
         <div
           v-if="item.product.origin_price === item.product.price"
-          class="text-secondary col-md-12 col-lg-4"
+          class="text-secondary col-md-12 col-lg-12 col-xl-4"
         >
           NT$ {{ item.product.origin_price }}
         </div>
-        <div v-else class="text-danger fw-bold col-md-12 col-lg-4">
+        <div v-else class="text-danger fw-bold col-md-12 col-lg-12 col-xl-4">
           NT$ {{ item.product.price }}
         </div>
       </div>
 
-      <div class="row m-0 align-items-center col-5 col-md-6 col-lg-4 col-xl-4">
-        <div class="d-flex col-12 col-md-6 col-lg-6 col-xl-6">
+      <div
+        class="row m-0 align-items-center col-11 col-sm-4 col-md-5 col-lg-5 col-xl-4"
+      >
+        <div class="py-2 d-flex col-12 col-md-6 col-lg-6 col-xl-6">
           <button
             @click="delOneQty(item)"
             :disabled="item.id === status.delLoadingItem"
             type="button"
-            class="btn btn-light rounded-0 rounded-start"
+            class="btn btn-light btn-sm rounded-0 rounded-start"
           >
             <i class="bi bi-dash-lg"></i>
           </button>
@@ -55,21 +59,21 @@
             @change="updateQtyOfInput(item)"
             v-model="item.qty"
             type="text"
-            class="form-control text-center rounded-0"
+            class="form-control form-control-sm text-center rounded-0"
           />
 
           <button
             @click="addOneToCart(item)"
             :disabled="item.id === status.addLoadingItem"
             type="button"
-            class="btn btn-light rounded-0 rounded-end"
+            class="btn btn-light btn-sm rounded-0 rounded-end"
           >
             <i class="bi bi-plus-lg"></i>
           </button>
         </div>
 
         <div
-          class="mt-3 text-center text-md-end col-12 col-md-6 col-lg-6 col-xl-6"
+          class="my-2 text-center text-md-end col-12 col-md-6 col-lg-6 col-xl-6"
         >
           NT$ {{ item.total }}
         </div>
