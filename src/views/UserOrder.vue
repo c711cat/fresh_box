4e1ac06196891159494a1c46bfce712ae14f44a6
c711cat <c711cat@gmail.com>
2024-04-13 22:33:20 +0800

    加上按下確認付款後，會改成出現查看訂單及繼續逛逛的按鈕

--- src/views/UserOrder.vue
+++ src/views/UserOrder.vue
@@ -14,12 +14,24 @@
     <router-link v-if="order.is_paid" to="/user/order-list" class="border-0">
       <button type="button" class="btn btn-outline-success px-4">
         查看訂單
+    <div class="text-end col-12 col-lg-7 pe-4 pb-5">
+      <router-link
+        v-if="order.is_paid"
+        to="/user/user-products"
+        class="border-0"
+      >
+        <button type="button" class="btn btn-outline-primary px-4 me-3">
+          繼續逛逛
+        </button>
+      </router-link>
+      <router-link v-if="order.is_paid" to="/user/order-list" class="border-0">
+        <button type="button" class="btn btn-outline-success px-4">
+          查看訂單
+        </button>
+      </router-link>
+      <button v-else @click="toPay" type="button" class="btn btn-danger px-4">
+        確認付款
       </button>
-    </router-link>
-
-    <button v-else @click="toPay" type="button" class="btn btn-danger px-4">
-      確認付款
-    </button>
   </div>
 </template>
 
