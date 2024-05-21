79a1402745ec361dc0dde49c76a1d838f8518ad9
c711cat <c711cat@gmail.com>
2024-05-21 13:44:08 +0800

    調整購物車在xs及sm以上寬度的顯示

--- src/components/UserNavbar.vue
+++ src/components/UserNavbar.vue
@@ -15,7 +15,7 @@
       </span>
       <!-- xs 可見：字體縮小 及 調整視覺置中 -->
       <span
-        class="fs-5 pt-1 logoText d-block d-sm-none d-md-none d-lg-none d-xl-none d-xxl-none"
+        class="fs-5 pt-2 logoText d-block d-sm-none d-md-none d-lg-none d-xl-none d-xxl-none"
       >
         FRESH BOX
       </span>
@@ -33,15 +33,16 @@
         <i class="bi bi-list fs-1 px-2 navbarText"></i>
       </button>
       <router-link to="/cart" class="nav-link d-flex flex-row-reverse">
+        <!-- 購物車 sm 以上版面 -->
         <div
           style="height: 75px"
-          class="h-100 d-flex flex-column align-items-center"
+          class="h-100 d-flex flex-column align-items-center text-center d-none d-sm-block d-md-block d-lg-block d-xl-block d-xxl-block"
         >
           <div class="h-auto position-relative">
             <i class="bi bi-cart2 fs-2 iconLink"></i>
             <span
               v-if="carts.length >= 1"
-              class="position-absolute translate-middle badge rounded-pill numInCart"
+              class="xs_cart_num_position translate-middle badge rounded-pill numInCart"
             >
               {{ carts.length }}
               <span class="visually-hidden">cart items</span>
@@ -49,10 +50,34 @@
           </div>
 
           <h5 class="m-0 d-flex align-items-center">
-            <span class="amountText badge"
+            <span class="amountText sm_amountText badge"
               >NT$ {{ $filters.currency(undiscountedAmount) }}
             </span>
           </h5>
+        </div>
+        <!-- 購物車 xs 以下版面 -->
+        <div
+          class="h-100 d-flex flex-column align-items-center pe-1 d-block d-sm-none d-md-none d-lg-none d-xl-none d-xxl-none"
+        >
+          <div class="h-auto position-relative">
+            <i class="bi bi-cart2 fs-2 iconLink"></i>
+            <span
+              v-if="carts.length >= 1"
+              class="sm_cart_num_position translate-middle badge rounded-pill numInCart"
+            >
+              {{ carts.length }}
+              <span class="visually-hidden">cart items</span>
+            </span>
+          </div>
+
+          <div
+            class="m-0 d-flex flex-column justify-content-center align-items-center"
+          >
+            <span class="amountText xs_amountText badge">NT$ </span>
+            <span class="amountText xs_amountText badge"
+              >{{ $filters.currency(undiscountedAmount) }}
+            </span>
+          </div>
         </div>
       </router-link>
     </div>
@@ -226,9 +251,16 @@ export default {
   color: rgb(249, 196, 6);
 }
 
-.position-absolute {
-  top: 13px;
-  left: 38px;
+.xs_cart_num_position {
+  position: absolute;
+  top: 14px;
+  left: 70px;
+}
+
+.sm_cart_num_position {
+  position: absolute;
+  top: 10px;
+  left: 28px;
 }
 
 .form-control:focus {
@@ -242,10 +274,17 @@ export default {
 }
 
 .amountText {
-  width: 120px;
+  padding: 5px 0px;
   color: rgb(249, 196, 6);
 }
 
+.sm_amountText {
+  width: 120px;
+}
+
+.xs_amountText {
+  width: 60px;
+}
 .numInCart {
   background-color: rgb(249, 196, 6);
   color: #000;
@@ -253,5 +292,8 @@ export default {
 
 .isCurrentNavbarItem {
   color: rgb(249, 196, 6);
+}
+* {
+  // border: 1px solid;
 }
 </style>
