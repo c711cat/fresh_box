c80855bee298d493565cdfa2ac5d4a9ef012da99
c711cat <c711cat@gmail.com>
2024-05-31 15:51:26 +0800

    更改admin navbar樣式及排版

--- src/components/AdminNavbar.vue
+++ src/components/AdminNavbar.vue
@@ -1,94 +1,115 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <nav
-    class="bg-body-tertiary navbar navbar-expand-lg fixed-top row m-0 col-12"
-  >
-    <div class="container-fluid col-lg-auto">
+  <nav class="navbar navbar-expand-md bg-light fixed-top">
+    <div
+      class="col-12 px-3 d-flex justify-content-between align-items-center flex-wrap"
+    >
       <button
-        class="navbar-toggler border-0"
+        class="navbar-toggler border-0 col-auto"
         type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#user_navbarNav"
-        aria-controls="user_navbarNav"
-        aria-expanded="false"
+        data-bs-toggle="offcanvas"
+        data-bs-target="#offcanvasNavbar"
+        aria-controls="offcanvasNavbar"
         aria-label="Toggle navigation"
       >
-        <i class="bi bi-list fs-1 px-2"></i>
+        <i class="bi bi-list fs-2 px-2"></i>
       </button>
-      <router-link
-        to="/"
-        class="logoText text-decoration-none text-center fs-4"
+      <div
+        class="offcanvas offcanvas-start"
+        tabindex="-1"
+        id="offcanvasNavbar"
+        aria-labelledby="offcanvasNavbarLabel"
       >
-        FRESH BOX
-      </router-link>
-    </div>
-    <div class="collapse navbar-collapse" id="user_navbarNav">
-      <ul class="navbar-nav col-lg-10">
-        <li class="nav-item">
+        <div class="offcanvas-header px-4">
           <router-link
-            to="/dashboard/admin's-products"
-            :class="{
-              isCurrentNavbarItem:
-                this.$route.path === `/dashboard/admin's-products`,
-            }"
-            class="nav-link text-center navbarText px-4"
-            >產品清單
-          </router-link>
-        </li>
-        <li class="nav-item">
-          <router-link
-            to="/dashboard/coupons"
-            :class="{
-              isCurrentNavbarItem: this.$route.path === '/dashboard/coupons',
-            }"
-            class="nav-link text-center navbarText px-4"
-            >優惠券
-          </router-link>
-        </li>
-        <li class="nav-item">
-          <router-link
-            to="/dashboard/order-list"
-            :class="{
-              isCurrentNavbarItem: this.$route.path === '/dashboard/order-list',
-            }"
-            class="nav-link text-center navbarText px-4"
-            >訂單
-          </router-link>
-        </li>
-        <li class="nav-item">
-          <a
-            @click.prevent="logOut"
-            class="logOutLink nav-link text-center navbarText px-4"
-            >登出</a
+            to="/"
+            class="offcanvas-title logoText text-decoration-none fs-4"
+            id="offcanvasNavbarLabel"
           >
-        </li>
-        <li class="nav-item col-lg-5 col-xl-4">
-          <form class="px-4" role="search">
-            <input
-              v-if="this.$route.path === `/dashboard/admin's-products`"
-              v-model="productSearchText"
-              class="form-control me-2"
-              type="search"
-              placeholder="Search for products"
-              aria-label="Search"
-            />
-            <input
-              v-if="this.$route.path === '/dashboard/order-list'"
-              v-model="orderSearchText"
-              class="form-control me-2"
-              type="search"
-              placeholder="Search for name on orders"
-              aria-label="Search"
-            />
-          </form>
-        </li>
-      </ul>
+            FRESH BOX
+          </router-link>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="offcanvas"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="offcanvas-body ps-5">
+          <ul class="navbar-nav flex-grow-1 pe-3">
+            <li class="nav-item">
+              <router-link
+                to="/dashboard/admin's-products"
+                class="nav-link"
+                :class="{
+                  isCurrentNavbarItem:
+                    this.$route.path === `/dashboard/admin's-products`,
+                }"
+              >
+                產品清單
+              </router-link>
+            </li>
+            <li class="nav-item">
+              <router-link
+                to="/dashboard/coupons"
+                class="nav-link"
+                :class="{
+                  isCurrentNavbarItem:
+                    this.$route.path === '/dashboard/coupons',
+                }"
+              >
+                優惠券
+              </router-link>
+            </li>
+            <li class="nav-item">
+              <router-link
+                to="/dashboard/order-list"
+                class="nav-link"
+                :class="{
+                  isCurrentNavbarItem:
+                    this.$route.path === '/dashboard/order-list',
+                }"
+              >
+                訂單
+              </router-link>
+            </li>
+            <li class="nav-item">
+              <a @click.prevent="logOut" class="nav-link" href="#"> 登出 </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <router-link
+        v-if="currentWidth > 275"
+        to="/"
+        class="logoTextLink navbar-brand text-center p-0 m-0"
+      >
+        <span class="logoText text-center">FRESH BOX</span>
+      </router-link>
+      <form class="col-2 col-sm-3" role="search">
+        <input
+          v-if="this.$route.path === `/dashboard/admin's-products`"
+          v-model="productSearchText"
+          class="form-control"
+          type="search"
+          placeholder="Search for products"
+          aria-label="Search"
+        />
+        <input
+          v-if="this.$route.path === '/dashboard/order-list'"
+          v-model="orderSearchText"
+          class="form-control"
+          type="search"
+          placeholder="Search for name on orders"
+          aria-label="Search"
+        />
+      </form>
     </div>
   </nav>
 </template>
 
 <script>
-import Collapse from "bootstrap/js/dist/collapse";
+import Offcanvas from "bootstrap/js/dist/offcanvas";
 export default {
   data() {
     return {
@@ -100,6 +121,7 @@ export default {
       orderPage: 1,
       orders: [],
       orderSearchResult: [],
+      currentWidth: 1000,
     };
   },
   inject: ["emitter"],
@@ -188,44 +210,46 @@ export default {
           this.$pushMsg.status404(error.response.data.message);
         });
     },
+    isCurrentWidth() {
+      this.currentWidth = window.innerWidth;
+    },
   },
   created() {
     this.getOrders();
-    const collapseElementList = document.querySelectorAll(".collapse");
-    this.adminNavbar = [...collapseElementList].map(
-      (collapseEl) => new Collapse(collapseEl)
+    const offcanvasElementList = document.querySelectorAll(".offcanvas");
+    this.adminNavbar = [...offcanvasElementList].map(
+      (offcanvasEl) => new Offcanvas(offcanvasEl)
     );
     this.getProducts();
+    setTimeout(() => {
+      this.isCurrentWidth();
+    }, 500);
   },
 };
 </script>
 
 <style lang="scss" scoped>
+.logoTextLink {
+  width: 1px;
+  position: absolute;
+  right: 50%;
+  display: flex;
+  justify-content: center;
+}
 .logoText {
-  width: 170px;
+  width: fit-content;
   color: #000000a6;
   font-family: "Times New Roman", Times, serif;
 }
 
-.logoText:hover {
-  cursor: pointer;
-  color: black;
-}
-
-.navbarText:hover {
-  font-weight: bold;
-}
-
-.navbar-toggler:focus {
-  box-shadow: none;
-}
-
+.logoText:hover,
+.nav-link:hover,
 .bi-list:hover {
   color: black;
 }
 
-.logOutLink:hover {
-  cursor: pointer;
+.navbar-toggler:focus {
+  box-shadow: none;
 }
 
 .isCurrentNavbarItem {
