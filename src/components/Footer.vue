2dd333fb0755c04b3443ad7c7e0c4f18a4b4e063
c711cat <c711cat@gmail.com>
2024-06-26 13:01:42 +0800

    修改登入的UI

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -17,9 +17,9 @@
         <div style="min-width: 145px" class="text-sm-end py-2 col">
           <router-link
             to="/login"
-            class="text-decoration-none rounded-pill text-center bg-black text-light py-2 px-4 adminLogin"
+            class="text-decoration-none rounded-1 text-center bg-black text-light py-2 px-3 adminLogin"
           >
-            後台登入
+            後台
           </router-link>
         </div>
 
