f62d12e73ce3fb63a3221c1e270808f9bf44b0b6
c711cat <c711cat@gmail.com>
2024-06-26 16:05:13 +0800

    刪除沒用到的部分

--- src/components/Observer.vue
+++ src/components/Observer.vue
@@ -1,8 +1,6 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <div ref="target" class="w-100 text-center text-secondary target">
-    商品載入中 ⋯
-  </div>
+  <div ref="target" class="w-100 text-center text-secondary">商品載入中 ⋯</div>
 </template>
 
 <script setup>
