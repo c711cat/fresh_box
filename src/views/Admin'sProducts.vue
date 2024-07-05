466a4cd5a78625d2968d32691cd9f68bb227a6b4
c711cat <c711cat@gmail.com>
2024-07-05 15:32:06 +0800

    若是新增商品時，則自動先帶入一個空字串在注意事項中，Modal才會出現注意事項的欄位，比較容易讓人會直接填寫進去

--- src/views/Admin'sProducts.vue
+++ src/views/Admin'sProducts.vue
@@ -134,7 +134,7 @@ export default {
     },
     openModal(isNew, item) {
       if (isNew) {
-        this.tempProduct = {};
+        this.tempProduct = { notes: [""] };
       } else {
         this.tempProduct = { ...item };
       }
