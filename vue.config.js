af04efbd1cd5ce5705ebbc883b6e6117ca6904ac
c711cat <c711cat@gmail.com>
2024-08-05 14:45:56 +0800

    vue.config.js 的 publicPath 調整成官方文件的建議

--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,8 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/fresh_box/',
 });
 
 module.exports = {
-  chainWebpack: (config) => {
-    config.plugin("define").tap((definitions) => {
-      Object.assign(definitions[0], {
-        __VUE_OPTION_API__: "true",
-        __VUE_PROD_DEVTOOLS__: "false",
-        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: "false",
-      });
-      return definitions;
-    });
-  },
-
-  publicPath: '/fresh_box/'
+  publicPath: process.env.NODE_ENV === "production" ? "/fresh_box/" : "/",
 };
