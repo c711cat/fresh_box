fa283bbb5dba9bc1f7c1f76f092e0154d6fbc279
c711cat <c711cat@gmail.com>
2024-08-06 13:57:20 +0800

    import 的路徑使用「@」取代每一層的點點符號

--- src/router/index.js
+++ src/router/index.js
@@ -3,69 +3,69 @@ import { createRouter, createWebHashHistory } from "vue-router";
 const routes = [
   {
     path: "/login",
-    component: () => import("../views/Login.vue"),
+    component: () => import("@/views/Login.vue"),
   },
   {
     path: "/dashboard",
-    component: () => import("../views/backend/AdminDashboard.vue"),
+    component: () => import("@/views/backend/AdminDashboard.vue"),
     children: [
       {
         path: "admin's-products",
-        component: () => import("../views/backend/Admin'sProducts.vue"),
+        component: () => import("@/views/backend/Admin'sProducts.vue"),
       },
       {
         path: "coupons",
-        component: () => import("../views/backend/Coupons.vue"),
+        component: () => import("@/views/backend/Coupons.vue"),
       },
       {
         path: "order-list",
-        component: () => import("../views/backend/AdminOrderList.vue"),
+        component: () => import("@/views/backend/AdminOrderList.vue"),
       },
       {
         path: "admin-QA",
-        component: () => import("../views/backend/AdminQA.vue"),
+        component: () => import("@/views/backend/AdminQA.vue"),
       },
     ],
   },
   {
     path: "/",
-    component: () => import("../views/frontend/UserDashboard.vue"),
+    component: () => import("@/views/frontend/UserDashboard.vue"),
     children: [
       {
         path: "",
-        component: () => import("../views/frontend/Home.vue"),
+        component: () => import("@/views/frontend/Home.vue"),
       },
       {
         path: "user-products",
-        component: () => import("../views/frontend/UserProducts.vue"),
+        component: () => import("@/views/frontend/UserProducts.vue"),
       },
       {
         path: "user-products/:currentCategory",
-        component: () => import("../views/frontend/UserProducts.vue"),
+        component: () => import("@/views/frontend/UserProducts.vue"),
       },
       {
         path: "product/:id",
-        component: () => import("../views/frontend/ProductDetails.vue"),
+        component: () => import("@/views/frontend/ProductDetails.vue"),
       },
       {
         path: "cart",
-        component: () => import("../views/frontend/CartView.vue"),
+        component: () => import("@/views/frontend/CartView.vue"),
       },
       {
         path: "favorite",
-        component: () => import("../views/frontend/UserProducts.vue"),
+        component: () => import("@/views/frontend/UserProducts.vue"),
       },
       {
         path: "order/:orderId",
-        component: () => import("../views/frontend/UserOrder.vue"),
+        component: () => import("@/views/frontend/UserOrder.vue"),
       },
       {
         path: "order-list",
-        component: () => import("../views/frontend/UserOrderList.vue"),
+        component: () => import("@/views/frontend/UserOrderList.vue"),
       },
       {
         path: "QA",
-        component: () => import("../views/frontend/UserQA.vue"),
+        component: () => import("@/views/frontend/UserQA.vue"),
       },
     ],
   },
