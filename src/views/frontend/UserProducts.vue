6dcb4249bdc231c1d04452222e4219c7e33dc5a5
c711cat <c711cat@gmail.com>
2024-08-30 16:20:01 +0800

    加上側邊欄位作為類別選單

--- src/views/frontend/UserProducts.vue
+++ src/views/frontend/UserProducts.vue
@@ -1,195 +1,186 @@
 <template>
   <LoadingView v-if="isLoading" />
-  <main
-    v-else
-    class="col-11 col-sm-10 col-md-8 col-lg-8 col-xl-10 col-xxl-9 row mt-0 mb-3 mx-auto productsContainer"
-  >
-    <ul class="mt-1 nav d-flex align-items-center">
-      <li @click="goToAllProducts" class="nav-item nav-link border-0">
-        所有產品
-      </li>
-      <li>／</li>
-      <li class="nav-item dropdown">
-        <a
-          class="nav-link dropdown-toggle border-0"
-          data-bs-toggle="dropdown"
-          href="#"
-          role="button"
-          aria-expanded="false"
+  <main v-else class="col-11 col-xxl-9 d-flex mx-auto productsContainer">
+    <div class="border col-2">
+      <section class="list-group">
+        <buttom
+          @click="goToAllProducts"
+          class="list-group-item list-group-item-action"
+          :class="{ active: $route.path === '/user-products' }"
+          :aria-current="$route.path === '/user-products'"
+          >所有類別
+        </buttom>
+        <buttom
+          @click="goToTheCategory(item)"
+          v-for="item in categoryList"
+          :key="item"
+          class="list-group-item list-group-item-action"
+          :class="{ active: $route.params.currentCategory === item }"
+          :aria-current="$route.params.currentCategory === item"
         >
-          {{ currentCategory }}
-        </a>
-        <ul class="dropdown-menu">
-          <li
-            @click="goToTheCategory(item)"
-            v-for="item in categoryList"
-            :key="item"
-            class="dropdown-item"
-          >
-            {{ item }}
-          </li>
-        </ul>
-      </li>
-    </ul>
-    <section v-if="noResults" class="mt-4">
-      <h3>查無相符商品</h3>
-    </section>
-    <section v-if="noFavorites" class="mt-4">
-      <h3>目前無收藏的商品</h3>
-    </section>
-    <section
-      v-for="item in allProducts"
-      :key="item.id"
-      class="col-6 col-sm-6 col-md-6 col-lg-4 col-xl-3 col-xxl-3"
-    >
-      <div class="card my-3 mx-auto">
-        <h3
-          v-if="isMyFavorite(item)"
-          @click="delMyFavorite(item)"
-          class="bi bi-suit-heart-fill delmyFavoriteIcon position-absolute z-1"
-        ></h3>
+          {{ item }}
+        </buttom>
+      </section>
+    </div>
+    <div class="col-10 border">
+      <section v-if="noResults" class="mt-4">
+        <h3>查無相符商品</h3>
+      </section>
+      <section v-if="noFavorites" class="mt-4">
+        <h3>目前無收藏的商品</h3>
+      </section>
+      <div class="d-flex flex-wrap col-12">
+        <section v-for="item in allProducts" :key="item.id" class="col-3 px-3">
+          <div class="card my-3 mx-auto">
+            <h3
+              v-if="isMyFavorite(item)"
+              @click="delMyFavorite(item)"
+              class="bi bi-suit-heart-fill delmyFavoriteIcon position-absolute z-1"
+            ></h3>
 
-        <h3
-          v-else
-          @click="addMyFavorite(item)"
-          class="bi bi-suit-heart myFavoriteIcon position-absolute z-1"
-        ></h3>
-        <div class="position-relative">
-          <img
-            @click="goToProduct(item)"
-            :src="item.imageUrl"
-            class="imgBody card-img-top"
-          />
-        </div>
-
-        <div class="card-body d-flex flex-column justify-content-between">
-          <div>
-            <div class="d-flex justify-content-between">
-              <h5
-                class="card-title d-none d-sm-block d-md-block d-lg-block d-xl-block d-xxl-block"
-              >
-                {{ item.title }}
-              </h5>
-              <h5
-                class="card-title xs_title_text d-block d-sm-none d-md-none d-lg-none d-xl-none d-xxl-none"
-              >
-                {{ item.title }}
-              </h5>
-
-              <span
-                v-if="item.buyQty >= 1"
-                class="badge text-bg-danger rounded-pill fs-6 d-none d-sm-none d-md-block d-lg-block d-xl-block d-xxl-block"
-              >
-                {{ item.buyQty }}
-              </span>
-              <span
-                v-if="item.buyQty >= 1"
-                class="badge text-bg-danger rounded-pill xs_text d-block d-sm-block d-md-none d-lg-none d-xl-none d-xxl-none"
-              >
-                {{ item.buyQty }}
-              </span>
+            <h3
+              v-else
+              @click="addMyFavorite(item)"
+              class="bi bi-suit-heart myFavoriteIcon position-absolute z-1"
+            ></h3>
+            <div class="position-relative">
+              <img
+                @click="goToProduct(item)"
+                :src="item.imageUrl"
+                class="imgBody card-img-top"
+              />
             </div>
 
-            <p
-              class="card-text d-none d-sm-block d-md-block d-lg-block d-xl-block d-xxl-block"
-            >
-              {{ item.content }} / {{ item.unit }}
-            </p>
-            <p
-              class="card-text xs_text d-block d-sm-none d-md-none d-lg-none d-xl-none d-xxl-none"
-            >
-              {{ item.content }} / {{ item.unit }}
-            </p>
-            <div
-              class="priceContainer d-flex justify-content-between flex-wrap mb-2"
-            >
-              <strong
-                class="col-12 col-sm-auto card-text text-secondary fs-6 d-none d-sm-block d-md-block d-lg-block d-xl-block d-xxl-block"
-                :class="{
-                  'text-decoration-line-through':
-                    item.price !== item.origin_price,
-                }"
+            <div class="card-body d-flex flex-column justify-content-between">
+              <div>
+                <div class="d-flex justify-content-between">
+                  <h5
+                    class="card-title d-none d-sm-block d-md-block d-lg-block d-xl-block d-xxl-block"
+                  >
+                    {{ item.title }}
+                  </h5>
+                  <h5
+                    class="card-title xs_title_text d-block d-sm-none d-md-none d-lg-none d-xl-none d-xxl-none"
+                  >
+                    {{ item.title }}
+                  </h5>
+
+                  <span
+                    v-if="item.buyQty >= 1"
+                    class="badge text-bg-danger rounded-pill fs-6 d-none d-sm-none d-md-block d-lg-block d-xl-block d-xxl-block"
+                  >
+                    {{ item.buyQty }}
+                  </span>
+                  <span
+                    v-if="item.buyQty >= 1"
+                    class="badge text-bg-danger rounded-pill xs_text d-block d-sm-block d-md-none d-lg-none d-xl-none d-xxl-none"
+                  >
+                    {{ item.buyQty }}
+                  </span>
+                </div>
+
+                <p
+                  class="card-text d-none d-sm-block d-md-block d-lg-block d-xl-block d-xxl-block"
+                >
+                  {{ item.content }} / {{ item.unit }}
+                </p>
+                <p
+                  class="card-text xs_text d-block d-sm-none d-md-none d-lg-none d-xl-none d-xxl-none"
+                >
+                  {{ item.content }} / {{ item.unit }}
+                </p>
+                <div
+                  class="priceContainer d-flex justify-content-between flex-wrap mb-2"
+                >
+                  <strong
+                    class="col-12 col-sm-auto card-text text-secondary fs-6 d-none d-sm-block d-md-block d-lg-block d-xl-block d-xxl-block"
+                    :class="{
+                      'text-decoration-line-through':
+                        item.price !== item.origin_price,
+                    }"
+                  >
+                    NT$ {{ $filters.currency(item.origin_price) }}
+                  </strong>
+
+                  <strong
+                    class="col-12 col-sm-auto card-text text-secondary xs_text d-block d-sm-none d-md-none d-lg-none d-xl-none d-xxl-none"
+                    :class="{
+                      'text-decoration-line-through':
+                        item.price !== item.origin_price,
+                    }"
+                  >
+                    NT$ {{ $filters.currency(item.origin_price) }}
+                  </strong>
+
+                  <strong
+                    v-if="item.price !== item.origin_price"
+                    class="col-12 col-sm-auto card-text text-danger fs-6 d-none d-sm-block d-md-block d-lg-block d-xl-block d-xxl-block"
+                    >NT$ {{ $filters.currency(item.price) }}
+                  </strong>
+
+                  <strong
+                    v-if="item.price !== item.origin_price"
+                    class="col-12 col-sm-auto card-text text-danger xs_text d-block d-sm-none d-md-none d-lg-none d-xl-none d-xxl-none"
+                    >NT$ {{ $filters.currency(item.price) }}
+                  </strong>
+                </div>
+              </div>
+
+              <div
+                class="btn-group w-100"
+                role="group"
+                aria-label="Default button group"
               >
-                NT$ {{ $filters.currency(item.origin_price) }}
-              </strong>
+                <button
+                  @click="delOne(item)"
+                  :disabled="!item.buyQty || item.id === status.delLoadingItem"
+                  type="button"
+                  class="btn btn-light w-50"
+                >
+                  <div
+                    v-if="item.id === status.delLoadingItem"
+                    class="spinner-border text-dark spinner-grow-sm"
+                    role="status"
+                  >
+                    <span class="visually-hidden">Loading...</span>
+                  </div>
+                  <i v-else class="bi bi-dash-lg"></i>
+                </button>
 
-              <strong
-                class="col-12 col-sm-auto card-text text-secondary xs_text d-block d-sm-none d-md-none d-lg-none d-xl-none d-xxl-none"
-                :class="{
-                  'text-decoration-line-through':
-                    item.price !== item.origin_price,
-                }"
-              >
-                NT$ {{ $filters.currency(item.origin_price) }}
-              </strong>
-
-              <strong
-                v-if="item.price !== item.origin_price"
-                class="col-12 col-sm-auto card-text text-danger fs-6 d-none d-sm-block d-md-block d-lg-block d-xl-block d-xxl-block"
-                >NT$ {{ $filters.currency(item.price) }}
-              </strong>
-
-              <strong
-                v-if="item.price !== item.origin_price"
-                class="col-12 col-sm-auto card-text text-danger xs_text d-block d-sm-none d-md-none d-lg-none d-xl-none d-xxl-none"
-                >NT$ {{ $filters.currency(item.price) }}
-              </strong>
+                <button
+                  @click="addCart(item)"
+                  :disabled="item.id === status.addLoadingItem"
+                  type="button"
+                  class="btn btn-light w-50"
+                >
+                  <div
+                    v-if="item.id === status.addLoadingItem"
+                    class="spinner-border text-dark spinner-grow-sm"
+                    role="status"
+                  >
+                    <span class="visually-hidden">Loading...</span>
+                  </div>
+                  <i v-else class="bi bi-plus-lg"></i>
+                </button>
+              </div>
             </div>
           </div>
-
-          <div
-            class="btn-group w-100"
-            role="group"
-            aria-label="Default button group"
-          >
-            <button
-              @click="delOne(item)"
-              :disabled="!item.buyQty || item.id === status.delLoadingItem"
-              type="button"
-              class="btn btn-light w-50"
-            >
-              <div
-                v-if="item.id === status.delLoadingItem"
-                class="spinner-border text-dark spinner-grow-sm"
-                role="status"
-              >
-                <span class="visually-hidden">Loading...</span>
-              </div>
-              <i v-else class="bi bi-dash-lg"></i>
-            </button>
-
-            <button
-              @click="addCart(item)"
-              :disabled="item.id === status.addLoadingItem"
-              type="button"
-              class="btn btn-light w-50"
-            >
-              <div
-                v-if="item.id === status.addLoadingItem"
-                class="spinner-border text-dark spinner-grow-sm"
-                role="status"
-              >
-                <span class="visually-hidden">Loading...</span>
-              </div>
-              <i v-else class="bi bi-plus-lg"></i>
-            </button>
-          </div>
+        </section>
+        <ObserverView
+          v-if="
+            pagination.current_page < pagination.total_pages &&
+            $route.path === '/user-products'
+          "
+          @is-in-view="handleIsInView"
+          @is-outside-view="handleIsOutsideView"
+        />
+        <div
+          v-if="pagination.current_page === pagination.total_pages"
+          class="w-100 text-secondary text-center"
+        >
+          <p class="mb-0 noMore">已無更多商品</p>
         </div>
       </div>
-    </section>
-    <ObserverView
-      v-if="
-        pagination.current_page < pagination.total_pages &&
-        $route.path === '/user-products'
-      "
-      @is-in-view="handleIsInView"
-      @is-outside-view="handleIsOutsideView"
-    />
-    <div
-      v-if="pagination.current_page === pagination.total_pages"
-      class="w-100 text-secondary text-center"
-    >
-      <p class="mb-0 noMore">已無更多商品</p>
     </div>
   </main>
 </template>
@@ -384,8 +375,12 @@ export default {
       }
     },
     goToTheCategory(category) {
-      this.$router.push(`/user-products/${category}`);
-      this.chooseCategory(category);
+      if (category === "所有類別") {
+        this.goToAllProducts();
+      } else {
+        this.$router.push(`/user-products/${category}`);
+        this.chooseCategory(category);
+      }
     },
     handleIsInView() {
       this.isInView = true;
@@ -617,5 +612,15 @@ img {
   100% {
     opacity: 0;
   }
+}
+
+.list-group-item:hover {
+  cursor: pointer;
+}
+
+.list-group-item.active {
+  background-color: #fff;
+  color: #ccaf3c;
+  border: 1px solid #dee2e6;
 }
 </style>
