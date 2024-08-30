cfc86b0730c84eee28dbd2f6a0b63e0894d468cd
c711cat <c711cat@gmail.com>
2024-08-30 11:52:01 +0800

    修改排版

--- src/components/LoadingView.vue
+++ src/components/LoadingView.vue
@@ -1,11 +1,9 @@
 <template>
-  <div class="laoderWrap">
-    <div
-      class="loader position-absolute top-50 start-50 translate-middle d-flex justify-content-center align-items-center"
-    >
-      <span class="loader1 position-absolute"></span>
-      <span class="loader2 position-absolute"></span>
-      <span class="loader3 position-absolute"></span>
+  <div class="laoderWrap d-flex justify-content-center align-items-center">
+    <div class="loadingBox d-flex justify-content-center align-items-center">
+      <span class="loader1"></span>
+      <span class="loader2"></span>
+      <span class="loader3"></span>
       <text class="text-dark">Loading...</text>
     </div>
   </div>
@@ -13,42 +11,43 @@
 
 <style lang="scss" scoped>
 .laoderWrap {
-  position: relative;
   height: 100vh;
-  width: 100vw;
+  width: 100%;
   background-color: #00000055;
   z-index: 1031;
 }
 
-.loader {
+.loadingBox {
+  position: relative;
   width: 120px;
   height: 120px;
 }
 
-.loader span {
-  width: 100%;
-  height: 100%;
+span {
+  position: absolute;
+  width: 120px;
+  height: 120px;
   top: 0;
   left: 0;
-  pointer-events: none;
-  animation: loading 6s linear infinite;
 }
 
-.loader .loader1 {
+.loader1 {
   border: 1px solid #f9c406;
   border-radius: 66% 34% 69% 31% / 31% 71% 29% 69%;
+  animation: loading 6s linear infinite;
 }
-
-.loader .loader2 {
+.loader2 {
   border: 1px solid #f9c406;
-  animation-direction: reverse;
+
   border-radius: 73% 27% 68% 32% / 31% 71% 29% 75%;
+  animation: loading 6s linear infinite;
+  animation-direction: reverse;
 }
 
-.loader .loader3 {
+.loader3 {
   border: 1px solid #f9c406;
-  animation-duration: 10s;
   border-radius: 37% 63% 27% 73% / 67% 43% 57% 33%;
+  animation: loading 10s linear infinite;
 }
 
 @keyframes loading {
