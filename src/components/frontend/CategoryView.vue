bb5b46bc5f07182c7aa32c9fffc65012d218a808
c711cat <c711cat@gmail.com>
2024-09-04 15:08:45 +0800

    加上背景色做區隔，上下留空間加大，避免太擁擠

--- src/components/frontend/CategoryView.vue
+++ src/components/frontend/CategoryView.vue
@@ -1,113 +1,115 @@
 <template>
-  <section
-    class="categoryWrap bg-light-gray row m-0 flex-wrap justify-content-center align-items-center"
-  >
-    <router-link
-      :to="`/user-products/${categoryList.veleafy_vegetable}`"
-      data-aos="zoom-in"
-      data-aos-duration="600"
-      class="m-1 categoryLink text-secondary col-2 col-lg-1"
+  <div class="bg-light">
+    <main
+      class="mx-auto categoryWrap py-4 d-flex flex-wrap col-12 col-sm-9 col-lg-9 justify-content-center align-items-center"
     >
-      <img
-        class="categoryImg"
-        src="@/assets/img/leafy_vegetable.png"
-        alt="leafy_vegetable_img"
-      />
+      <router-link
+        :to="`/user-products/${categoryList.veleafy_vegetable}`"
+        data-aos="zoom-in"
+        data-aos-duration="600"
+        class="m-1 categoryLink text-secondary col col-sm"
+      >
+        <img
+          class="categoryImg"
+          src="@/assets/img/leafy_vegetable.png"
+          alt="leafy_vegetable_img"
+        />
 
-      <p
-        class="mb-0 d-none d-sm-none d-md-block d-lg-block d-xl-block d-xxl-block"
+        <p
+          class="mb-0 d-none d-sm-none d-md-block d-lg-block d-xl-block d-xxl-block"
+        >
+          {{ categoryList.veleafy_vegetable }}
+        </p>
+        <p
+          class="mb-0 smFontSize d-block d-sm-block d-md-none d-lg-none d-xl-none d-xxl-none"
+        >
+          {{ categoryList.veleafy_vegetable }}
+        </p>
+      </router-link>
+      <router-link
+        :to="`/user-products/${categoryList.melon_root_bulb}`"
+        data-aos="zoom-in"
+        data-aos-duration="600"
+        class="m-1 categoryLink text-secondary col col-sm"
       >
-        {{ categoryList.veleafy_vegetable }}
-      </p>
-      <p
-        class="mb-0 smFontSize d-block d-sm-block d-md-none d-lg-none d-xl-none d-xxl-none"
-      >
-        {{ categoryList.veleafy_vegetable }}
-      </p>
-    </router-link>
-    <router-link
-      :to="`/user-products/${categoryList.melon_root_bulb}`"
-      data-aos="zoom-in"
-      data-aos-duration="600"
-      class="m-1 categoryLink text-secondary col-2 col-lg-1"
-    >
-      <img
-        class="categoryImg"
-        src="@/assets/img/melon_root_bulb.png"
-        alt="melon_root_bulb_img"
-      />
-      <p
-        class="mb-0 d-none d-sm-none d-md-block d-lg-block d-xl-block d-xxl-block"
-      >
-        {{ categoryList.melon_root_bulb }}
-      </p>
+        <img
+          class="categoryImg"
+          src="@/assets/img/melon_root_bulb.png"
+          alt="melon_root_bulb_img"
+        />
+        <p
+          class="mb-0 d-none d-sm-none d-md-block d-lg-block d-xl-block d-xxl-block"
+        >
+          {{ categoryList.melon_root_bulb }}
+        </p>
 
-      <p
-        class="mb-0 smFontSize d-block d-sm-block d-md-none d-lg-none d-xl-none d-xxl-none"
+        <p
+          class="mb-0 smFontSize d-block d-sm-block d-md-none d-lg-none d-xl-none d-xxl-none"
+        >
+          {{ categoryList.melon_root_bulb }}
+        </p>
+      </router-link>
+      <router-link
+        :to="`/user-products/${categoryList.mushroom}`"
+        data-aos="zoom-in"
+        data-aos-duration="600"
+        class="m-1 categoryLink text-secondary col col-sm"
       >
-        {{ categoryList.melon_root_bulb }}
-      </p>
-    </router-link>
-    <router-link
-      :to="`/user-products/${categoryList.mushroom}`"
-      data-aos="zoom-in"
-      data-aos-duration="600"
-      class="m-1 categoryLink text-secondary col-2 col-lg-1"
-    >
-      <img
-        class="categoryImg"
-        src="@/assets/img/mushroom.png"
-        alt="mushroom_img"
-      />
-      <p
-        class="mb-0 d-none d-sm-none d-md-block d-lg-block d-xl-block d-xxl-block"
-      >
-        {{ categoryList.mushroom }}
-      </p>
+        <img
+          class="categoryImg"
+          src="@/assets/img/mushroom.png"
+          alt="mushroom_img"
+        />
+        <p
+          class="mb-0 d-none d-sm-none d-md-block d-lg-block d-xl-block d-xxl-block"
+        >
+          {{ categoryList.mushroom }}
+        </p>
 
-      <p
-        class="mb-0 smFontSize d-block d-sm-block d-md-none d-lg-none d-xl-none d-xxl-none"
+        <p
+          class="mb-0 smFontSize d-block d-sm-block d-md-none d-lg-none d-xl-none d-xxl-none"
+        >
+          {{ categoryList.mushroom }}
+        </p>
+      </router-link>
+      <router-link
+        :to="`/user-products/${categoryList.fruit}`"
+        data-aos="zoom-in"
+        data-aos-duration="600"
+        class="m-1 categoryLink text-secondary col col-sm"
       >
-        {{ categoryList.mushroom }}
-      </p>
-    </router-link>
-    <router-link
-      :to="`/user-products/${categoryList.fruit}`"
-      data-aos="zoom-in"
-      data-aos-duration="600"
-      class="m-1 categoryLink text-secondary col-2 col-lg-1"
-    >
-      <img class="categoryImg" src="@/assets/img/fruit.png" alt="fruit_img" />
-      <p
-        class="mb-0 d-none d-sm-none d-md-block d-lg-block d-xl-block d-xxl-block"
+        <img class="categoryImg" src="@/assets/img/fruit.png" alt="fruit_img" />
+        <p
+          class="mb-0 d-none d-sm-none d-md-block d-lg-block d-xl-block d-xxl-block"
+        >
+          {{ categoryList.fruit }}
+        </p>
+        <p
+          class="mb-0 smFontSize d-block d-sm-block d-md-none d-lg-none d-xl-none d-xxl-none"
+        >
+          {{ categoryList.fruit }}
+        </p>
+      </router-link>
+      <router-link
+        :to="`/user-products/${categoryList.spice}`"
+        data-aos="zoom-in"
+        data-aos-duration="600"
+        class="m-1 categoryLink text-secondary col col-sm"
       >
-        {{ categoryList.fruit }}
-      </p>
-      <p
-        class="mb-0 smFontSize d-block d-sm-block d-md-none d-lg-none d-xl-none d-xxl-none"
-      >
-        {{ categoryList.fruit }}
-      </p>
-    </router-link>
-    <router-link
-      :to="`/user-products/${categoryList.spice}`"
-      data-aos="zoom-in"
-      data-aos-duration="600"
-      class="m-1 categoryLink text-secondary col-2 col-lg-1"
-    >
-      <img class="categoryImg" src="@/assets/img/spice.png" alt="spice_img" />
-      <p
-        class="mb-0 d-none d-sm-none d-md-block d-lg-block d-xl-block d-xxl-block"
-      >
-        {{ categoryList.spice }}
-      </p>
-      <p
-        class="mb-0 smFontSize d-block d-sm-block d-md-none d-lg-none d-xl-none d-xxl-none"
-      >
-        {{ categoryList.spice }}
-      </p>
-    </router-link>
-  </section>
+        <img class="categoryImg" src="@/assets/img/spice.png" alt="spice_img" />
+        <p
+          class="mb-0 d-none d-sm-none d-md-block d-lg-block d-xl-block d-xxl-block"
+        >
+          {{ categoryList.spice }}
+        </p>
+        <p
+          class="mb-0 smFontSize d-block d-sm-block d-md-none d-lg-none d-xl-none d-xxl-none"
+        >
+          {{ categoryList.spice }}
+        </p>
+      </router-link>
+    </main>
+  </div>
 </template>
 <script>
 export default {
@@ -141,15 +143,17 @@ export default {
   justify-content: center;
   align-items: center;
   text-decoration: none;
+  min-width: 75px;
 }
 
 .categoryLink:hover {
-  border-radius: 5%;
-  background-color: rgba(249, 196, 6, 0.292);
+  border-radius: 6px;
+  background-color: #f9c40637;
   color: #000 !important;
+  filter: brightness(1.1);
 }
 
 .smFontSize {
-  font-size: 12px;
+  font-size: 14px;
 }
 </style>
