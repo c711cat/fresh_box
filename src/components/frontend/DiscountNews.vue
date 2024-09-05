b497003a4bdfc8d1ea38d4911a8177bc7109b3cc
c711cat <c711cat@gmail.com>
2024-09-05 16:07:48 +0800

    將優惠消息獨立一個元件製作，程式碼看起來感覺較簡單且單純

---
+++ src/components/frontend/DiscountNews.vue
@@ -0,0 +1,57 @@
+<template>
+  <main>
+    <div
+      class="position-relative d-flex justify-content-center align-items-center"
+    >
+      <img
+        class="newsImg object-fit-cover col-12 p-0"
+        data-aos="fade-zoom-in"
+        data-aos-easing="ease-out-circ"
+        data-aos-duration="2000"
+        src="@/assets/img/home_img2.jpg"
+        alt="homeImg"
+      />
+      <router-link
+        to="/user-products"
+        class="newsContent px-1 text-decoration-none text-black col-11 col-sm-9 col-lg-8 col-xl-6 d-flex flex-column justify-content-center align-items-center"
+        data-aos="zoom-in"
+        data-aos-easing="ease-out-sine"
+        data-aos-duration="900"
+      >
+        <div class="fs-1 fw-bold mb-3">全 館 滿 千 免 運</div>
+        <p class="fs-4 fw-bold text-center mb-4">
+          結帳代入
+          <span class="border border-secondary rounded px-1 py-1 fw-bold me-1">
+            10%off
+          </span>
+          享 9 折優惠
+        </p>
+        <p
+          class="fw-bold fs-5 mb-0 bg-primary border border-primary px-4 py-1 rounded goShopping"
+        >
+          去 逛 逛 吧 !
+        </p>
+      </router-link>
+    </div>
+  </main>
+</template>
+<style lang="scss" scoped>
+.newsImg {
+  height: 300px;
+}
+
+.newsContent {
+  position: absolute;
+  height: 250px;
+  background-color: rgba(252, 251, 251, 0.832);
+}
+
+.newsContent:hover {
+  filter: brightness(1.1);
+}
+
+.newsContent .goShopping:hover {
+  background-color: #d4bb59 !important;
+  border: 1px solid #d1b750 !important;
+}
+</style>
--- src/views/frontend/HomeView.vue
+++ src/views/frontend/HomeView.vue
@@ -20,37 +20,14 @@
     >
       暢銷商品
     </h3>
-
     <BestSellers />
   </section>
-
-  <section class="row col-12 mx-0 justify-content-center align-items-center">
-    <img
-      class="newsImg object-fit-cover col-lg-12 p-0"
-      data-aos="fade-zoom-in"
-      data-aos-easing="ease-out-circ"
-      data-aos-duration="2000"
-      src="@/assets/img/home_img2.jpg"
-      alt="homeImg"
-    />
-    <router-link
-      to="/user-products"
-      class="newsContent text-decoration-none text-black col-11 col-sm-9 col-lg-8 col-xl-6 d-flex flex-column justify-content-center align-items-center"
-      data-aos="zoom-in"
-      data-aos-easing="ease-out-sine"
-      data-aos-duration="900"
-    >
-      <div class="fs-1 fw-lighter mb-3">全館滿千免運</div>
-      <div class="fs-4 fw-lighter text-center mb-3">
-        結帳代入
-        <span class="border border-secondary px-2 py-1">10%off</span>
-        享 9 折優惠
-      </div>
-      <div class="goShopping fw-lighter fs-5">去 逛 逛</div>
-    </router-link>
+  <!-- 優惠消息 -->
+  <section class="mb-5 col-12">
+    <DiscountNews />
   </section>
-
-  <section class="mx-auto py-5 col-11 col-xl-9">
+  <!-- 精選商品 -->
+  <section class="mx-auto mb-5 col-11 col-xl-9">
     <h3
       class="mb-3 border-bottom border-dark"
       data-aos="fade-in"
@@ -71,70 +48,12 @@
 import CategoryView from "@/components/frontend/CategoryView.vue";
 import BestSellers from "@/components/frontend/BestSellers.vue";
 import SwiperImgs from "@/components/frontend/SwiperImgs.vue";
+import DiscountNews from "@/components/frontend/DiscountNews.vue";
 export default {
   data() {
-    return {
-      bestSellerProducts: [
-        {
-          title: "台灣水蜜桃",
-          imgUrl:
-            "https://images.unsplash.com/photo-1595124245030-41448b199d6d?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-          content: "600g±10% / 盒",
-          origin_price: 600,
-          price: 500,
-          id: "-NsfVCnYqjTKonXqR4cO",
-        },
-        {
-          title: "無籽黑葡萄",
-          imgUrl:
-            "https://images.unsplash.com/photo-1601275868399-45bec4f4cd9d?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-          content: "1.8kg±10% / 盒",
-          origin_price: 699,
-          price: 569,
-          id: "-Nsg6Th1g1I7OzbPJF1g",
-        },
-        {
-          title: "空運櫻桃",
-          imgUrl:
-            "https://images.unsplash.com/photo-1595657241488-468423581c23?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-          content: "1kg±10% / 盒",
-          origin_price: 1200,
-          price: 1000,
-          id: "-NsfbLG9v4NLUQwba1YJ",
-        },
-        {
-          title: "蘆筍",
-          imgUrl:
-            "https://images.unsplash.com/photo-1629875235136-737fef945cfd?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-          content: "200g±10% / 盒",
-          origin_price: 150,
-          price: 120,
-          id: "-Ntoe6-WbDJY9ChStYOs",
-        },
-      ],
-    };
+    return {};
   },
-  components: { CategoryView, BestSellers, SwiperImgs },
-  inject: ["emitter"],
-  methods: {
-    addCart(item) {
-      const addItem = { product_id: item.id, qty: 1 };
-      const api = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/cart`;
-      this.$http
-        .post(api, { data: addItem })
-        .then((res) => {
-          if (res.data.success) {
-            this.$pushMsg.status200(res, "已加入購物車");
-            this.emitter.emit("updateProductInCart");
-          } else {
-            this.$pushMsg.status200(res, "加入購物車失敗");
-          }
-        })
-        .catch((error) => {
-          this.$pushMsg.status404(error.response.data.message);
-        });
-    },
-  },
+  components: { CategoryView, BestSellers, DiscountNews, SwiperImgs },
 };
 </script>
 
@@ -143,44 +62,5 @@ export default {
   width: 100%;
   height: 100vh;
   object-fit: cover;
-}
-
-.straightLine {
-  width: 1px;
-  background-color: black;
-  height: 180px;
-}
-
-.bestSellersImg {
-  width: 100%;
-  height: 300px;
-  object-fit: cover;
-}
-
-.bestSellersImg:hover {
-  box-shadow: 0px 8px 10px rgba(36, 35, 35, 0.511) !important;
-}
-
-.newsImg {
-  height: 300px;
-}
-
-.newsContent {
-  height: 250px;
-  background-color: rgba(252, 251, 251, 0.832);
-  position: absolute;
-}
-
-.goShopping {
-  opacity: 0;
-  transition: opacity 0.3s ease;
-}
-
-.newsContent:hover {
-  box-shadow: 0px 8px 10px rgba(36, 35, 35, 0.511);
-}
-
-.newsContent:hover .goShopping {
-  opacity: 1;
 }
 </style>
